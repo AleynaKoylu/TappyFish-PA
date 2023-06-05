fcb4dca5157ecd335161d3ce84196de8e2f647c3
AleynaKöylü <aleynakoylu4@gmail.com>
2023-06-05 18:37:49 +0300

    GameOverPanel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static Vector2 buttonleft; 
     public static bool GameOver;
+    public GameObject GameOverPanel;
     private void Awake()
     {
         buttonleft = Camera.main.ScreenToViewportPoint(new Vector2(0, 0));
@@ -14,6 +16,15 @@ public class GameManager : MonoBehaviour
     public void GameOverr()
     {
         GameOver = true;
+        if (GameOver == true)
+        {
+            GameOverPanel.SetActive(true);
+
+        }
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     void Start()
     {
@@ -22,6 +33,6 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+     
     }
 }
